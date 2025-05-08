cf9b4ba703f7980f8a15269183ec32641a571db2
suchitra-su <suchitrajana0327@gamil.com>
2025-05-08 20:09:51 +0530

    Complete cutter name ui
    Complete Cutter Name UI

--- mobile/src/app/pages/pages.routes.ts
+++ mobile/src/app/pages/pages.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { Empty } from './empty/empty';
+import { CutterNameComponent } from './master/cutter-name/cutter-name.component';
 
 export default [
+    { path: 'cutter-name', component: CutterNameComponent },
     { path: 'empty', component: Empty },
-    { path: '**', redirectTo: '/notfound' }
+    { path: '**', redirectTo: '/notfound' },
+
 ] as Routes;
