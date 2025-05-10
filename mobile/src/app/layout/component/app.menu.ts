768f3eaf0677a10aed5ca86b6ae13dc42bb8756b
Rana-su <madhumitarana554@gmail.com>
2025-05-10 20:29:31 +0530

    order-master create

--- mobile/src/app/layout/component/app.menu.ts
+++ mobile/src/app/layout/component/app.menu.ts
@@ -78,7 +78,7 @@ export class AppMenu {
                     {
                         label: 'Order Sheet',
                         icon: 'pi pi-fw pi-file',
-                        routerLink: ['/order-sheet']
+                        routerLink: ['pages/order-sheet']
                     },
                     {
                         label: 'Cutting',
--- mobile/src/app/pages/pages.routes.ts
+++ mobile/src/app/pages/pages.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Empty } from './empty/empty';
 import { UserMasterComponent } from './user-master/user-master.component';
 import { CreateUsersComponent } from './create-users/create-users.component';
+import { OrdersheetComponent } from './ordersheet/ordersheet.component';
 
 export default [
     { path: 'empty', component: Empty },
     { path: 'user-master', component: UserMasterComponent },
     { path: 'create-user', component:CreateUsersComponent },
+    { path: 'order-sheet', component:OrdersheetComponent },
     { path: '**', redirectTo: '/notfound' }
 ] as Routes;
