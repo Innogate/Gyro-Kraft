ba4cb1e114743eda4c36b3c16c8f4e8a638a4047
Rana-su <madhumitarana554@gmail.com>
2025-05-10 21:51:51 +0530

    order-master image

--- mobile/src/app/pages/ordersheet/ordersheet.component.ts
+++ mobile/src/app/pages/ordersheet/ordersheet.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { DialogModule } from 'primeng/dialog';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ordersheet',
-  imports: [ReactiveFormsModule, DialogModule],
+  imports: [ReactiveFormsModule, DialogModule, CommonModule],
   templateUrl: './ordersheet.component.html',
   styleUrl: './ordersheet.component.scss'
 })
 export class OrdersheetComponent {
   displayProfile:boolean = false;
+  imageUrls: string[] = [];  // To store the image URLs
   productForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
@@ -30,6 +32,25 @@ export class OrdersheetComponent {
       deadlineDate: [null],
     });
   }
+
+
+ 
+
+  // Handle  more file showing
+onFilesSelected(event: any): void {
+  const files = event.target.files;  
+    
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    const reader = new FileReader();
+
+    reader.onload = (e: any) => {
+      this.imageUrls.push(e.target.result);
+    };
+    reader.readAsDataURL(file);
+    console.log(this.imageUrls)
+  }
+}
 
 
 
