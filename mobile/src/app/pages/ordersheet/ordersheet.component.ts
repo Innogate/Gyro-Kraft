fda5563dddd5e8a4bce7ce85037960c947eb11f9
tuhin-su <tuhinsubhraghara29195@gmail.com>
2025-05-18 13:34:29 -0400

    New UI added

--- mobile/src/app/pages/ordersheet/ordersheet.component.ts
+++ mobile/src/app/pages/ordersheet/ordersheet.component.ts
@@ -1,75 +1,211 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { DialogModule } from 'primeng/dialog';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
+
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { FileUploadModule } from 'primeng/fileupload';
+import { InputTextModule } from 'primeng/inputtext';
+import { CalendarModule } from 'primeng/calendar';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { TextareaModule } from 'primeng/textarea';
 
 @Component({
   selector: 'app-ordersheet',
-  imports: [ReactiveFormsModule, DialogModule, CommonModule],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    DialogModule,
+    DropdownModule,
+    FileUploadModule,
+    InputTextModule,
+    CalendarModule,
+    ButtonModule,
+    TableModule,
+    InputNumberModule,
+    TextareaModule,
+  ],
   templateUrl: './ordersheet.component.html',
-  styleUrl: './ordersheet.component.scss'
+  styleUrls: ['./ordersheet.component.scss'],
 })
 export class OrdersheetComponent {
-  displayProfile: boolean = false;
-  imageUrls: string[] = [];
-  selectedFiles: File[] = [];
-  productForm: FormGroup;
+  orderForm: FormGroup;
+  ageGroupOptions = [
+    { label: 'Infant', value: 'Infant' },
+    { label: 'Toddler', value: 'Toddler' },
+    { label: 'Child', value: 'Child' },
+    { label: 'Teen', value: 'Teen' },
+    { label: 'Adult', value: 'Adult' },
+  ];
 
+  articleDialogVisible = false;
+  selectedPOIndex = -1;
 
   constructor(private fb: FormBuilder) {
-    this.productForm = this.fb.group({
-      uniqueId: [''],
-      date: [null],
-      buyer: [''],
-      styleNo: [''],
-      brand: [''],
-      season: [''],
-      ageGroup: [''],
+    this.orderForm = this.fb.group({
+      uniqueId: ['', Validators.required],
+      date: [null, Validators.required],
+      buyer: ['', Validators.required],
+      styleNo: ['', Validators.required],
+      brand: ['', Validators.required],
+      season: ['', Validators.required],
+      ageGroup: [null, Validators.required],
       shipmentDate: [null],
       pattern: [''],
       printing: [''],
-      documents: [null],
-      stepsRequired: [''],
-      remark: [''],
+      steps: [''],
       deadlineDate: [null],
+      remark: [''],
+      documents: [[]], // store file info here
 
-
-      combo: [''],
-      proColor: [''],
-      fabricQuality: [''],
-      supplier: [''],
-      preemie: [''],
-      nb: [''],
-      total_quantity:[''],
+      poQty: this.fb.array([]),
+      fabricBOM: this.fb.array([]),
+      accessoriesBOM: this.fb.array([]),
     });
   }
 
+  // getters for easy access to form controls
+  get f() {
+    return this.orderForm.controls;
+  }
 
+  get poQty(): FormArray {
+    return this.orderForm.get('poQty') as FormArray;
+  }
 
+  get fabricBOM(): FormArray {
+    return this.orderForm.get('fabricBOM') as FormArray;
+  }
 
-  // Handle  more file showing
-  onFilesSelected(event: any): void {
-    const files: FileList = event.target.files;
+  get accessoriesBOM(): FormArray {
+    return this.orderForm.get('accessoriesBOM') as FormArray;
+  }
 
-    for (let i = 0; i < files.length; i++) {
-      const file = files[i];
-      const reader = new FileReader();
+  // Add PO Qty group
+  addPOQty() {
+    this.poQty.push(
+      this.fb.group({
+        combo: [''],
+        proColor: [''],
+        fabricQuality: [''],
+        supplier: [''],
+        preemie: [0],
+        nb: [0],
+        totalQty: [0],
+        articles: this.fb.array([]),
+      })
+    );
+  }
 
-      reader.onload = (e: any) => {
-        this.imageUrls.push(e.target.result);
-        this.selectedFiles.push(file);
-        this.productForm.patchValue({ documents: this.selectedFiles });
-      };
+  removePOQty(index: number) {
+    this.poQty.removeAt(index);
+  }
 
-      reader.readAsDataURL(file);
+  // Articles form array inside selected PO Qty
+  getArticlesArray(index: number): FormArray {
+    return this.poQty.at(index).get('articles') as FormArray;
+  }
+
+  // Article dialog form for currently selected PO Qty
+  get selectedPOArticlesGroup(): FormGroup {
+    if (this.selectedPOIndex === -1) return this.fb.group({});
+    return this.poQty.at(this.selectedPOIndex) as FormGroup;
+  }
+
+  get articlesArray(): FormArray {
+    if (this.selectedPOIndex === -1) return this.fb.array([]);
+    return this.getArticlesArray(this.selectedPOIndex);
+  }
+
+  openArticleDialog(poIndex: number) {
+    this.selectedPOIndex = poIndex;
+    this.articleDialogVisible = true;
+  }
+
+  addArticle() {
+    if (this.selectedPOIndex === -1) return;
+    this.articlesArray.push(
+      this.fb.group({
+        ageGroup: [null],
+        articleNo: [''],
+        qty: [0],
+        gender: [''],
+        productPhotos: [[]], // file info
+      })
+    );
+  }
+
+  removeArticle(index: number) {
+    this.articlesArray.removeAt(index);
+  }
+
+  // BOM Fabric
+  addFabricBOM() {
+    this.fabricBOM.push(
+      this.fb.group({
+        description: [''],
+        fabric: [''],
+        colour: [''],
+        tpx: [''],
+      })
+    );
+  }
+
+  removeFabricBOM(index: number) {
+    this.fabricBOM.removeAt(index);
+  }
+
+  // BOM Accessories
+  addAccessoryBOM() {
+    this.accessoriesBOM.push(
+      this.fb.group({
+        particular: [''],
+        specification: [''],
+        shadeNo: [''],
+        consumption: [0],
+        supplier: [''],
+        status: [''],
+      })
+    );
+  }
+
+  removeAccessoryBOM(index: number) {
+    this.accessoriesBOM.removeAt(index);
+  }
+
+  // File upload handlers
+  onDocUpload(event: any) {
+    // For demo, just store files info array
+    const files = event.files;
+    this.orderForm.patchValue({ documents: files });
+  }
+
+  onProductPhotoUpload(event: any, articleIndex: number) {
+    if (this.selectedPOIndex === -1) return;
+    const files = event.files;
+    const articles = this.getArticlesArray(this.selectedPOIndex);
+    const currentPhotos = articles.at(articleIndex).get('productPhotos')?.value || [];
+    articles.at(articleIndex).patchValue({
+      productPhotos: [...currentPhotos, ...files],
+    });
+  }
+
+  submitOrder() {
+    if (this.orderForm.invalid) {
+      this.orderForm.markAllAsTouched();
+      return;
     }
+
+    console.log('Submitted Order:', this.orderForm.value);
+    alert('Order submitted! Check console for details.');
   }
-  removeFile(index: number): void {
-    this.imageUrls.splice(index, 1);
-    this.selectedFiles.splice(index, 1);
-    this.productForm.patchValue({ documents: this.selectedFiles });
-  }
-
-
-
 }
