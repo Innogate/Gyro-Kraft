9d24e7f1d3f357599c3ddad56dbacab20f1d60dc
tuhin-su <tuhinsubhraghara29195@gmail.com>
2025-05-19 10:44:03 -0400

    OTP SCREEN AND AUTH ADDED

--- mobile/src/app/pages/pages.routes.ts
+++ mobile/src/app/pages/pages.routes.ts
@@ -4,12 +4,13 @@ import { UserMasterComponent } from './user-master/user-master.component';
 import { CreateUsersComponent } from './create-users/create-users.component';
 import { CutterNameComponent } from './master/cutter-name/cutter-name.component';
 import { OrdersheetComponent } from './ordersheet/ordersheet.component';
+import { AuthGuard } from './auth/auth.guard';
 
 export default [
-    { path: 'cutter-name', component: CutterNameComponent },
+    { path: 'cutter-name', component: CutterNameComponent, canActivate: [AuthGuard] },
     { path: 'empty', component: Empty },
-    { path: 'user-master', component: UserMasterComponent },
-    { path: 'create-user', component:CreateUsersComponent },
-    { path: 'order-sheet', component:OrdersheetComponent },
+    { path: 'user-master', component: UserMasterComponent, canActivate: [AuthGuard]  },
+    { path: 'create-user', component:CreateUsersComponent, canActivate: [AuthGuard]  },
+    { path: 'order-sheet', component:OrdersheetComponent, canActivate: [AuthGuard]  },
     { path: '**', redirectTo: '/notfound' }
 ] as Routes;
