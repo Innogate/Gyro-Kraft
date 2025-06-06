bd52a9afd5cbae31b195e17b4f75f000f8c39958
Amit Maity <amitmaity2024@gmail.com>
2025-06-06 11:17:55 +0530

    update jobbers

--- backend/config/config.php
+++ backend/config/config.php
@@ -1,21 +1,21 @@
 <?php
-return [
-    'db' => [
-        'host' => getenv('DB_HOST'),
-        'dbname' => getenv('DB_NAME'),
-        'user' => getenv('DB_USER'),
-        'password' => getenv('DB_PASSWORD')
-    ],
-    'jwt_secret' => getenv('JWT_SECRET')
-];
-
-
 // return [
 //     'db' => [
-//         'host' => 'localhost',
-//         'dbname' => 'test',
-//         'user' => 'root',
-//         'password' => ''
+//         'host' => getenv('DB_HOST'),
+//         'dbname' => getenv('DB_NAME'),
+//         'user' => getenv('DB_USER'),
+//         'password' => getenv('DB_PASSWORD')
 //     ],
-//     'jwt_secret' => 'your_jwt_secret_key'
+//     'jwt_secret' => getenv('JWT_SECRET')
 // ];
+
+
+return [
+    'db' => [
+        'host' => 'localhost',
+        'dbname' => 'test',
+        'user' => 'root',
+        'password' => ''
+    ],
+    'jwt_secret' => 'your_jwt_secret_key'
+];
